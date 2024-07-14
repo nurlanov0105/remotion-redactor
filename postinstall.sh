976846a423ced2a7ebd6141325aa0426341068f7
Nursultan <nurlanov0105@gmail.com>
2024-07-14 18:07:51 +0600

    fix err

---
+++ postinstall.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+apt-get update && apt-get install -y \
+  libnss3 \
+  libatk1.0-0 \
+  libatk-bridge2.0-0 \
+  libcups2 \
+  libdrm2 \
+  libxkbcommon0 \
+  libxcomposite1 \
+  libxrandr2 \
+  libgbm1 \
+  libpango1.0-0 \
+  libcairo2 \
+  libasound2 \
+  libxshmfence1 \
+  libnss3-tools
